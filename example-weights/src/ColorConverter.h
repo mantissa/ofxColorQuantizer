f1aa5ef9476e4363cda101d32af81092de8a894e
stc <stc@binaura.net>
2017-06-20 13:55:14 +0200

    calculating chroma

---
+++ example-weights/src/ColorConverter.h
@@ -0,0 +1,13 @@
+//
+//  ColorConverter.h
+//  example-weights
+//
+//  Created by stc on 2017. 06. 20..
+//
+//
+
+#ifndef ColorConverter_h
+#define ColorConverter_h
+
+
+#endif /* ColorConverter_h */
